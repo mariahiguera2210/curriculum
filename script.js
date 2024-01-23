dd61a1034d5358dde6a6387f9166162f9c598374
mariahiguera2210 <angelica.higueram92@gmail.com>
2024-01-23 17:49:29 -0500

    se agrega boton Ver Proyectos Anteriores, y se crea en script.js lista de proyectos

--- script.js
+++ script.js
@@ -12,10 +12,56 @@ document.getElementById("addSkill").addEventListener("click", function () {
   }
 });
 
-document.getElementById("contactar").addEventListener("click", function(event){
-event.preventDefault();
-const emailValue = document.getElementById("email").value;
-if(emailValue){
-    alert("Gracias " + emailValue + "me pondré en contacto contigo pronto!")
-}
+document
+  .getElementById("contactar")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const emailValue = document.getElementById("email").value;
+    if (emailValue) {
+      alert("Gracias " + emailValue + " me pondré en contacto contigo pronto!");
+    } else {
+      alert("Ingresa email porfavor ");
+    }
+  });
+
+const certificaciones = [
+  "AWS Certified Cloud Practitioner",
+  "EF SET English Certificate C1/Adavance",
+  "Curso básico de JavaScript",
+];
+
+function mostrarCertificaciones() {
+  let ul = document.getElementById("listaCertificaciones");
+
+  for (let i = 0; i < certificaciones.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = certificaciones[i];
+    ul.appendChild(li);
+  }
+ 
+} window.onload = mostrarCertificaciones;
+
+const listaProyectos = [
+  "Tienda de Barrio", 
+  " Sistema de citas medicas",
+  " Hoja de vida"
+]
+
+document.getElementById("verProyectos").addEventListener("click",function(){
+ 
+  let numero =  parseInt(prompt("Del 1 al 3 cuántos proyectos deseas ver ingresa el número"))
+  let show;
+  for(let i = 0; i< listaProyectos.length; i++){
+    if(numero == 1){
+      show = alert(listaProyectos[0])
+    } else if(numero == 2){
+      show = alert(listaProyectos.slice(0,2))
+    }else if(numero == 3){
+      show = alert(listaProyectos)
+    }else if(numero > 3 || numero <= 0){
+      show = alert("Ingresa un numero entre 1 y 3")
+    }
+    return show;
+
+  }
 })
